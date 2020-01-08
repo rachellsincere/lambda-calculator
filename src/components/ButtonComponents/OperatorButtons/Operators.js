95adaa0e75283e25c2723ec27bbbb832fe7c681d
Rachell Sincere <rachellsincere@gmail.com>
2020-01-08 18:07:53 -0500

    imported useState hook and array data for each component  into Numbers.js, Operators.js, and Specials.js

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react"; //import useState hook
 
 //import any components needed
 
 //Import your array data to from the provided data file
+
+import { operators } from '../../../data';
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
